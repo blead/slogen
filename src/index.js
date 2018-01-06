7cef1ae3161cbdd88c9fd064adb511244b6cfae9
blead <blead010@gmail.com>
2018-01-06 14:11:42 +0700

    Implement random string generation

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 import 'normalize.css';
 import './index.css';
+
+const sloganText = document.getElementById('slogan-text');
+const generateButton = document.getElementById('generate-button');
+
+let data = {
+  slogan: ((element) => element.textContent)(sloganText)
+};
+
+const render = (prevData, nextData) => {
+  if(prevData.slogan != nextData.slogan) {
+    sloganText.textContent = nextData.slogan;
+  }
+};
+
+const handleClick = () => {
+  const nextData = {
+    ...data,
+    slogan: generateSlogan()
+  };
+  render(data, nextData);
+  data = nextData;
+};
+
+const generateSlogan = () => Math.random().toString(36).concat(Date.now().toString(36)).substring(2);
+
+generateButton.addEventListener('click', handleClick);
+
+
