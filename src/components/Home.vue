bd50267bc12bafafdf28d2123b1bfe703631ab01
blead <blead010@gmail.com>
2018-01-09 14:48:21 +0700

    Implement Home

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="hero is-fullheight">
+    <div class="hero-head">
+      <Nav/>
+    </div>
+    <div class="hero-body">
+      <Main/>
+    </div>
+    <div class="hero-foot">
+      <button class="button is-light is-radiusless is-fullwidth">
+        Configure
+      </button>
+    </div>
+  </section>
+</template>
+
+<script>
+import Nav from './Nav';
+import Main from './Main';
+
+export default {
+  name: 'Home',
+  components: {
+    Nav,
+    Main,
+  },
+};
+</script>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,14 +1,27 @@
 <template>
-  <div>
-    <h1>It works!</h1>
+  <div class="container has-text-centered">
+    <h1 class="title is-1">
+      {{ slogan }}
+    </h1>
+    <button class="button is-primary is-large" @click="handleClick">Generate</button>
   </div>
 </template>
 
 <script>
+const generateSlogan = () =>
+  Math.random().toString(36).concat(Date.now().toString(36)).substring(2);
+
 export default {
   name: 'Main',
+  data() {
+    return {
+      slogan: 'Generate your slogan',
+    };
+  },
+  methods: {
+    handleClick() {
+      this.slogan = generateSlogan();
+    },
+  },
 };
 </script>
-
-<style>
-</style>
