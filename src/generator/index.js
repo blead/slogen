5f442a36edf2413499cd8e0edefaf55643125181
peawyoyoyin <peawyoyoyin@gmail.com>
2018-01-10 00:42:57 +0700

    add logic for exclude/include tokens

--- src/generator/index.js
+++ src/generator/index.js
@@ -20,12 +20,38 @@ const generateSlogan = (config = {}) => { /* eslint-enable */
   const expectedLength = randomInt(minTokens, maxTokens + 1);
 
   const result = [];
-  const temp = tokens.slice();
+  const queryToken = tokens.slice();
+  if (config.exclude !== undefined) {
+    config.exclude.forEach((item) => {
+      if (queryToken.indexOf(item) >= 0) {
+        queryToken.splice(queryToken.indexOf(item), 1);
+      }
+    })
+  }
+
+  if (config.include !== undefined) {
+    config.include.forEach((item) => {
+      if (queryToken.indexOf(item) >= 0) {
+        queryToken.splice(queryToken.indexOf(item), 1);
+        result.push(item);
+      }
+    });
+  }
+
+  const temp = queryToken.slice();
   while (result.length < expectedLength) {
     const t = randomInt(temp.length);
     result.push(temp[t]);
     temp.splice(t, 1);
   }
+
+  Array(15).fill().forEach(() => {
+    const a = randomInt(result.length + 1);
+    const b = randomInt(result.length + 1);
+    const t = result[a];
+    result[a] = result[b];
+    result[b] = t;
+  });
 
   return result.join(' ');
 };
